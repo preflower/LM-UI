4a4cfd3aeffd3071778c71e25d8a8f3d8b0223c8
Lindberg.Lin <lindberg.lin@togetu.in>
2019-02-25 19:22:19 +0800

    init configuration
    初始化配置文件，Vue + Typescript + Jest

---
+++ packages/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    Welcome to use LM-UI
+    <l-button></l-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import LButton from '@/Template/index.vue';
+
+@Component({
+  components: {
+    LButton,
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
