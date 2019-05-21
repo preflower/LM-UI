a40b0ca34d24dbd951bca8a26dbe5d7808266963
lin <719206525@qq.com>
2019-05-21 15:18:48 +0800

    优化配置

--- packages/main.ts
+++ packages/main.ts
@@ -1,16 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue';
-import Toast from './toast/index.js';
+import App from './App.vue'
+import Toast from './toast/index.js'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(Toast);
+Vue.use(Toast)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
 })
