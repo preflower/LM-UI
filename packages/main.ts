062e997bc0718e6c7c6254df449c781463088433
lin <719206525@qq.com>
2019-05-21 15:19:28 +0800

    优化配置

--- packages/main.ts
+++ packages/main.ts
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App.vue'
-import Toast from './toast/index.js'
+import Toast from './toast/index.js';
 
 Vue.config.productionTip = false
 
