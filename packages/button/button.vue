e2262c92459d32ee97fd0e80d0c3d7675acc45b7
lin <719206525@qq.com>
2019-05-21 16:10:00 +0800

    优化配置

--- packages/button/button.vue
+++ packages/button/button.vue
@@ -1,25 +1,21 @@
 <template>
-<button
-  :type="type"
-  :class="btn_classes"
-  :disabled="disabled"
-  @click="handle_click">
-  <i class="iconfont" :class="icon" v-if="icon"></i>
-  <slot></slot>
-</button>
+  <button :type="type" :class="btn_classes" :disabled="disabled" @click="handle_click">
+    <i class="iconfont" :class="icon" v-if="icon"></i>
+    <slot></slot>
+  </button>
 </template>
 
-<script lang='ts'>
-import { Component, Prop, Emit, Vue } from 'vue-property-decorator';
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Button extends Vue {
-  @Prop({ default: false, type: Boolean }) inline!: Boolean;
-  @Prop({ default: false, type: Boolean }) disabled!: Boolean;
-  @Prop({ default: false, type: Boolean }) outline!: Boolean;
-  @Prop({ default: 'primary' }) type!: String;
-  @Prop({ default: 'button' }) nativeType!: String;
-  @Prop(String) icon?: String;
+  @Prop({ default: false, type: Boolean }) inline!: Boolean
+  @Prop({ default: false, type: Boolean }) disabled!: Boolean
+  @Prop({ default: false, type: Boolean }) outline!: Boolean
+  @Prop({ default: 'primary' }) type!: String
+  @Prop({ default: 'button' }) nativeType!: String
+  @Prop(String) icon?: String
 
   get btn_classes() {
     return [
@@ -29,22 +25,24 @@ export default class Button extends Vue {
         'lm-btn-block': !this.inline,
         'lm-btn-disabled': this.disabled,
         'lm-btn-outline': this.outline,
-      }
+      },
     ]
-  };
+  }
 
   @Emit('click')
-  handle_click (event: object) {
-    return event;
+  handle_click(event: object) {
+    return event
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import 'common/style/init.scss', 'common/style/variable.scss';
+
 @mixin btn-color($color, $second_color: #ffffff) {
   color: $second_color;
   background: $color;
+
   &.lm-btn-outline {
     color: $color;
     background: $second_color;
@@ -59,27 +57,34 @@ export default class Button extends Vue {
   cursor: pointer;
   border-radius: 4px;
   border: none;
+
   &-disabled {
     color: $gray-light;
-    opacity: .6;
+    opacity: 0.6;
   }
+
   &-block {
     display: block;
     width: 100%;
     padding: 12px 16px;
   }
+
   &--primary {
     @include btn-color($blue);
   }
+
   &--danger {
     @include btn-color($red);
   }
+
   &--warn {
     @include btn-color($orange);
   }
+
   &--dark {
     @include btn-color($dark);
   }
+
   &::before {
     content: '';
     position: absolute;
@@ -90,9 +95,11 @@ export default class Button extends Vue {
     opacity: 0;
     background-color: $dark-light;
   }
+
   &:active::before {
-    opacity: .2;
+    opacity: 0.2;
   }
+
   + .lm-btn {
     margin-top: 10px;
   }
